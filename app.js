26b1b2add805eb61bf98a4e248dd026facf1736d
Carlos Calleja <carloscallejasez@gmail.com>
2024-11-21 09:24:31 +0100

    feat: hola instead hello

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello world! Docker");
+  res.send("Hola world! Docker");
 });
 
 module.exports = app;
